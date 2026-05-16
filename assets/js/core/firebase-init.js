f3bf87ee6e80dae29b7f7ba485a5f95eb4c60b32
mr07110 <muhammad11rasul2011@gmail.com>
2026-05-16 11:29:10 +0500

    mening yangi o'zgarishlarim

--- assets/js/core/firebase-init.js
+++ assets/js/core/firebase-init.js
@@ -2,13 +2,17 @@ import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.7.
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import config from "../config.js";
+import { getConfig } from "../config.js";
+
+const config = await getConfig();
 
 const appName = "mrdev_main";
 let app = getApps().find(a => a.name === appName);
 if (!app) app = initializeApp(config.main, appName);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const rtdb = getDatabase(app);
 setPersistence(auth, browserLocalPersistence);
+
 export { app, auth, db, rtdb };
